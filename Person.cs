339acfb92c031522e8e2e90f605ec5472c3106ce
marc0dev0417 <98348202+marc0dev0417@users.noreply.github.com>
2022-10-30 21:29:53 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServices
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public Address address{ get; set; }
+    }
+
+    partial class Animal
+    {
+        public string id;
+    }
+    partial class Animal
+    {
+        public string name;
+    }
+    partial class Animal
+    {
+        public string age;
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace WebServices;
+
+using System.Text.Json;
+class WebServices
+{
+    public static async Task Main(){
+        string url = "https://jsonplaceholder.typicode.com/users";
+        HttpClient client = new HttpClient();
+
+        List<Person>? list = new List<Person>();
+
+        var response = await client.GetAsync(url);
+
+        if(response.IsSuccessStatusCode){
+            var content = await response.Content.ReadAsStringAsync();
+            list = JsonSerializer.Deserialize<List<Person>>(content); 
+        }else{
+            Console.WriteLine("Bad Request");
+        }
+
+        foreach(Person person in list)
+        {
+            Console.WriteLine($"Name is {person.name} and city is {person.address.city}");
+        }
+    }
+}
